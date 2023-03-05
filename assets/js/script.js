8dbc926f8074434538b1af85b8a8075f58395c54
anteCedens <jaksa.juric@yahoo.com.hr>
2023-03-05 23:50:16 +0000

    Copy code setup from Scrimba file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,44 @@
+let homeScore = 0;
+//document.getElementById("home-score").textContent = homeScore
+let guestScore = 0;
+//document.getElementById("guest-score").textContent = guestScore
 
+// "Home" buttons
+function plusOneHome() {
+    homeScore +=1;
+    document.getElementById("home-score").textContent = homeScore;
+}
+
+function plusTwoHome() {
+    homeScore +=2;
+    document.getElementById("home-score").textContent = homeScore;    
+}
+
+function plusThreeHome() {
+    homeScore += 3;
+    document.getElementById("home-score").textContent = homeScore;    
+}
+
+// "Guest" buttons
+function plusOneGuest() {
+    guestScore +=1;
+    document.getElementById("guest-score").textContent = guestScore;
+}
+
+function plusTwoGuest() {
+    guestScore +=2
+    document.getElementById("guest-score").textContent = guestScore    
+}
+
+function plusThreeGuest() {
+    guestScore += 3
+    document.getElementById("guest-score").textContent = guestScore    
+}
+
+// New Game
+function resetBoard() {
+    homeScore = 0
+    document.getElementById("home-score").textContent = homeScore
+    guestScore = 0
+    document.getElementById("guest-score").textContent = guestScore
+}
